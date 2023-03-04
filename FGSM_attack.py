3831684a6e023a1555829dae753269396677eff7
LiangXiaoyu <1020135941@qq.com>
2023-03-04 19:31:22 +0800

    implementation of LBFGS

--- FGSM_attack.py
+++ FGSM_attack.py
@@ -1,15 +1,11 @@
-import foolbox.utils
+import foolbox
 import numpy as np
 import torch
+import random
 import matplotlib.pyplot as plt
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import eagerpy as ep
-import foolbox as fb
+from matplotlib import use as mpl_use
 from foolbox.models import PyTorchModel
-from foolbox.attacks import FGSM
+from foolbox.v1.attacks import FGSM
 from foolbox.criteria import Misclassification
 from model import CNN
 
@@ -24,29 +20,40 @@ if __name__ == '__main__':
     clean_cnn = CNN().eval()
     # print(clean_cnn)
     clean_cnn.load_state_dict(torch.load("clean_CNN.pth"))
-    print("Load Pytorch Model from clean_CNN.pth")
+    print("Load Pytorch Model from clean_CNN.pth\n")
 
     # turn into foolbox model
-    fmodel = PyTorchModel(model=clean_cnn, bounds=(0, 255), num_classes=10)
+    fmodel = PyTorchModel(model=clean_cnn, bounds=(0, 1), num_classes=10)
 
     # FGSM attack
     criterion = Misclassification()
     attack = FGSM(model=fmodel, criterion=criterion)
 
-    image, label = foolbox.utils.samples(dataset='mnist', batchsize=1, bounds=(0, 255))
+    image, label = foolbox.utils.samples(dataset='mnist', batchsize=1, index=random.randint(0, 10000), bounds=(0, 1))
     print('true label: ', label)
-    pre_label = clean_cnn.forward(torch.tensor(image[np.newaxis, :]).to(device))
+    pre_label = clean_cnn(torch.tensor(image[np.newaxis, :]).to(device))
     print('prediction label: ', np.argmax(pre_label.detach().numpy()))
-    adversarial = attack(image, label)
+    adversarial = attack(input_or_adv=image, label=label, max_epsilon=0.1)
+    print('here')
+    adv_label = clean_cnn(torch.tensor(adversarial[np.newaxis, :]).to(device))
+    print('adv prediction label: ', np.argmax(adv_label.detach().numpy()))
 
-    # # plot the example
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(image)
-    #
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(adversarial)
-    #
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(adversarial - image)
+    # plot the example
+    mpl_use('MacOSX')
+
+    plt.subplot(1, 3, 1)
+    image = image.reshape(28, 28)
+    plt.imshow(image, cmap='gray')
+
+    plt.subplot(1, 3, 2)
+    perturbation = adversarial - image
+    perturbation = perturbation.reshape(28, 28)
+    plt.imshow(perturbation, cmap='gray')
+
+    plt.subplot(1, 3, 3)
+    adversarial = adversarial.reshape(28, 28)
+    plt.imshow(adversarial, cmap='gray')
+
+    plt.savefig('pics/FGSM_adv.png')
 
     print("Done!")
--- LBFGS_attack.py
+++ LBFGS_attack.py
@@ -3,12 +3,8 @@ import numpy as np
 import torch
 import random
 import matplotlib.pyplot as plt
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+from matplotlib import use as mpl_use
 from foolbox.models import PyTorchModel
-from foolbox import zoo
 from foolbox.v1.attacks import LBFGSAttack
 from foolbox.criteria import TargetClassProbability
 from model import CNN
@@ -27,29 +23,37 @@ if __name__ == '__main__':
     print("Load Pytorch Model from clean_CNN.pth\n")
 
     # turn into foolbox model
-    # fmodel = PyTorchModel(model=clean_cnn, bounds=(0, 255), num_classes=10)
-    fmodel = zoo.get_model(url="https://github.com/bethgelab/AnalysisBySynthesis")
+    fmodel = PyTorchModel(model=clean_cnn, bounds=(0, 1), num_classes=10)
 
     # L-BFGS attack
     target_class = 3
     criterion = TargetClassProbability(target_class=target_class, p=0.9)
     attack = LBFGSAttack(model=fmodel, criterion=criterion)
 
-    image, label = foolbox.utils.samples(dataset='mnist', batchsize=1, index=random.randint(0, 10000), bounds=(0, 255))
+    image, label = foolbox.utils.samples(dataset='mnist', batchsize=1, index=random.randint(0, 10000), bounds=(0, 1))
     print('true label: ', label)
-    pre_label = clean_cnn.forward(torch.tensor(image[np.newaxis, :]).to(device))
+    pre_label = clean_cnn(torch.tensor(image[np.newaxis, :]).to(device))
     print('prediction label: ', np.argmax(pre_label.detach().numpy()))
-    # print(image, label)
     adversarial = attack(input_or_adv=image, label=label)
+    adv_label = clean_cnn(torch.tensor(adversarial[np.newaxis, :]).to(device))
+    print('adv prediction label: ', np.argmax(adv_label.detach().numpy()))
 
-    # # plot the example
-    # plt.subplot(1, 3, 1)
-    # plt.imshow(image)
-    #
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(adversarial)
-    #
-    # plt.subplot(1, 3, 3)
-    # plt.imshow(adversarial - image)
+    # plot the example
+    mpl_use('MacOSX')
+
+    plt.subplot(1, 3, 1)
+    image = image.reshape(28, 28)
+    plt.imshow(image, cmap='gray')
+
+    plt.subplot(1, 3, 2)
+    perturbation = adversarial - image
+    perturbation = perturbation.reshape(28, 28)
+    plt.imshow(perturbation, cmap='gray')
+
+    plt.subplot(1, 3, 3)
+    adversarial = adversarial.reshape(28, 28)
+    plt.imshow(adversarial, cmap='gray')
+
+    plt.savefig('pics/LBFGS_adv.png')
 
     print("Done!")
